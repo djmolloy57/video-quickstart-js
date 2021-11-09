373b5a8d640e73e32986b0b349a416198d40097a
djmolloy57 <dj_molloy57@hotmail.com>
2021-11-09 09:45:32 +0000

    added examples screensharing and localvideo_snapshot functionality to quickstarts src index.js along with helper files and amended quickstart index.html to include screensharing and localvideo_snapshot

--- quickstart/src/index.js
+++ quickstart/src/index.js
@@ -148,3 +148,195 @@ async function selectMicrophone() {
 window.addEventListener('load', isSupported ? selectMicrophone : () => {
   showError($showErrorModal, new Error('This browser is not supported.'));
 });
+
+/*This code section is the code for screensharing - it uses the screenshare_helper file. The code here is taken from from the src index.js of example screensharing */
+const Prism = require('prismjs');
+const Video = require('twilio-video');
+
+//const getSnippet = require('../../util/getsnippet');
+const getSnippet = require('../util/getsnippet');
+
+//const getRoomCredentials = require('../../util/getroomcredentials');
+const getRoomCredentials = require('../util/getroomcredentials');
+
+const screensh_helpers = require('./screenshare_helpers');
+const createScreenTrack = screensh_helpers.createScreenTrack;
+
+const captureScreen = document.querySelector('button#capturescreen');
+const screenPreview = document.querySelector('video#screenpreview');
+const stopScreenCapture = document.querySelector('button#stopscreencapture');
+const remoteScreenPreview = document.querySelector('video.remote-screenpreview');
+
+(async function() {
+  // Load the code snippet.
+  
+  const snippet = await getSnippet('./helpers.js');
+  const pre = document.querySelector('pre.language-javascript');
+  
+  //pre.innerHTML = Prism.highlight(snippet, Prism.languages.javascript);
+
+
+  const logger = Video.Logger.getLogger('twilio-video');
+  logger.setLevel('silent');
+ 
+  // Connect Local Participant (screen-sharer) to a room
+  const localCreds = await getRoomCredentials();
+  alert("Hello getting credentials");
+  const roomLocal = await Video.connect(localCreds.token, {
+    tracks: []
+  });
+
+  // Connect Remote Participant (screen-viewer) to the room
+  const remoteCreds = await getRoomCredentials();
+  const roomRemote = await Video.connect(remoteCreds.token, {
+    name: roomLocal.name,
+    tracks: []
+  });
+
+  // Hide the "Stop Capture Screen" button.
+  stopScreenCapture.style.display = 'none';
+
+  // The LocalVideoTrack for your screen.
+  let screenTrack;
+
+  captureScreen.onclick = async function() {
+    try {
+
+      alert('YOU HAVE CLICKED ME!!');
+      // Create and preview your local screen.
+      screenTrack = await createScreenTrack(720, 1280);
+      screenTrack.attach(screenPreview);
+
+      // Publish screen track to room
+      await roomLocal.localParticipant.publishTrack(screenTrack);
+
+      // When screen sharing is stopped, unpublish the screen track.
+      screenTrack.on('stopped', () => {
+        if (roomLocal) {
+          roomLocal.localParticipant.unpublishTrack(screenTrack);
+        }
+        toggleButtons();
+      });
+
+      // Show the "Stop Capture Screen" button.
+      toggleButtons();
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
+  // Stop capturing your screen.
+  const stopScreenSharing = () => screenTrack.stop();
+
+  stopScreenCapture.onclick = stopScreenSharing;
+
+  // Remote Participant handles screen share track
+  if(roomRemote) {
+    roomRemote.on('trackPublished', publication => {
+      onTrackPublished('publish', publication, remoteScreenPreview);
+    });
+
+    roomRemote.on('trackUnpublished', publication => {
+      onTrackPublished('unpublish', publication, remoteScreenPreview);
+    });
+  }
+
+  // Disconnect from the Room on page unload.
+  window.onbeforeunload = function() {
+    if (roomLocal) {
+      roomLocal.disconnect();
+      roomLocal = null;
+    }
+    if (roomRemote) {
+      roomRemote.disconnect();
+      roomRemote = null;
+    }
+  };
+  alter('Where do I show?');
+}());
+
+function toggleButtons() {
+  captureScreen.style.display = captureScreen.style.display === 'none' ? '' : 'none';
+  stopScreenCapture.style.display = stopScreenCapture.style.display === 'none' ? '' : 'none';
+}
+
+function onTrackPublished(publishType, publication, view) {
+  if (publishType === 'publish') {
+    if (publication.track) {
+      publication.track.attach(view);
+    }
+
+    publication.on('subscribed', track => {
+      track.attach(view);
+    });
+  } else if (publishType === 'unpublish') {
+    if (publication.track) {
+      publication.track.detach(view);
+      view.srcObject = null;
+    }
+
+    publication.on('subscribed', track => {
+      track.detach(view);
+      view.srcObject = null;
+    });
+  }
+}
+/* end of the section which handles screensharing */
+
+/* the code below handles local video snapshot which is from the example local video snapshot src index.js - it calling helper file localvideoshapshot_helpers.js */ 
+var helpers_capt = require('./localvideoshapshot_helpers');
+
+
+var displayLocalVideo = helpers_capt.displayLocalVideo;
+var takeLocalVideoSnapshot = helpers_capt.takeLocalVideoSnapshot;
+
+var canvas = document.querySelector('.snapshot-canvas');
+alert('initializing local capture helper!!' + canvas);
+var img = document.querySelector('.snapshot-img');
+var takeSnapshot = document.querySelector('button#takesnapshot');
+var video = document.querySelector('video#videoinputpreview');
+
+let videoTrack;
+let el;
+
+// Show image or canvas
+window.onload = function() {
+  el = window.ImageCapture ? img : canvas;
+  el.classList.remove('hidden');
+  if(videoTrack) {
+    setSnapshotSizeToVideo(el, videoTrack);
+  }
+}
+
+// Set the canvas size to the video size.
+function setSnapshotSizeToVideo(snapshot, video) {
+  snapshot.width = video.dimensions.width;
+  snapshot.height = video.dimensions.height;
+}
+
+// Load the code snippet.
+//getSnippet('./helpers.js').then(function(snippet) {
+getSnippet('./localvideo_capture_helper.js').then(function(snippet) {
+  var pre = document.querySelector('pre.language-javascript');
+  //pre.innerHTML = Prism.highlight(snippet, Prism.languages.javascript);
+});
+
+// Request the default LocalVideoTrack and display it.
+displayLocalVideo(video).then(function(localVideoTrack) {
+  // Display a snapshot of the LocalVideoTrack on the canvas.
+  videoTrack = localVideoTrack;
+  takeSnapshot.onclick = function() {
+
+    alert('YOU HAVE CLICKED SCREEN CAPTURE!!');
+    setSnapshotSizeToVideo(el, localVideoTrack);
+    takeLocalVideoSnapshot(video, localVideoTrack, el);
+  };
+});
+
+// Resize the canvas to the video size whenever window is resized.
+window.onresize = function() {
+  setSnapshotSizeToVideo(el, videoTrack);
+};
+/*End of the above section on local video snapshot */
+
+
